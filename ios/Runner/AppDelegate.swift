d6fe732f1952282a261c709f258e527e6db2b000
Savio <savioskli@gmail.com>
2024-09-09 14:57:58 +0300

    appDelegate update

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,22 +3,30 @@ import Flutter
 import flutter_local_notifications
 import GoogleMaps
 
-
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    
+    // Provide API key for Google Maps services
     GMSServices.provideAPIKey("AIzaSyC6GQk3V5CGOj8g7n7UGygO4iriRqq5x5k")
+    
+    // Ensure that FlutterLocalNotificationsPlugin is correctly registered
     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-          GeneratedPluginRegistrant.register(with: registry)
+      GeneratedPluginRegistrant.register(with: registry)
     }
 
+    // Set the notification delegate if iOS 10.0 or later
     if #available(iOS 10.0, *) {
         UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
     }
+
+    // Register all other plugins, including flutter_inappwebview
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
